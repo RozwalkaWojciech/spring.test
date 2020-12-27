89ef4d562e19350c9d8205185b5cc7acc7278cd9
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-27 21:33:06 +0100

    init commit

---
+++ src/test/java/javer/spring/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package javer.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
