255882d709a36ba354430902a90802e70221b641
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-28 21:44:36 +0100

    add plainController

---
+++ src/main/java/javer/spring/test/controller/PlainController.java
@@ -0,0 +1,49 @@
+package javer.spring.test.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PlainController {
+
+    @GetMapping(
+            path = "/plain",
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    @ResponseBody
+    public String plainName(@RequestParam(name = "name") String name) {
+        return "Hello " + name;
+    }
+
+    @PostMapping(
+            path = "/plain",
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    @ResponseBody
+    public String plainLastName(@RequestParam(name = "lastName") String lastName) {
+        return "Hello " + lastName;
+    }
+
+    @PostMapping(
+            path = "/plain",
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    @ResponseBody
+    public String plainFullName(@RequestParam(name = "fullName") String fullName) {
+        return "Hello " + fullName;
+    }
+
+    @PostMapping(
+            path = "/plain",
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    @ResponseBody
+    public String plainFullName2(@RequestParam(name = "name") String name,
+                                 @RequestParam(name = "lastName") String lastName) {
+        return "Hello " + name + " " + lastName;
+    }
+}
