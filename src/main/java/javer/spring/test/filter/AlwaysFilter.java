771aecf1714f833a9bf4e461e02f8953d8ebd5e1
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-29 15:52:09 +0100

    filter for global path

---
+++ src/main/java/javer/spring/test/filter/AlwaysFilter.java
@@ -0,0 +1,20 @@
+package javer.spring.test.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(2)
+public class AlwaysFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        httpServletResponse.addHeader("X-Global", "global header");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
